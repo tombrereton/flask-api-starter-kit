b92f73cf42d1a5de572f260a4d2c68bb480ce72f
Tom Brereton <tom.brereton@exify.net>
2021-08-02 13:01:41 +0200

    Add tests for home route

---
+++ test/route/test_home.py
@@ -0,0 +1,22 @@
+from assertpy import assert_that
+import pytest
+
+import app
+
+
+@pytest.fixture
+def client():
+    client = app.create_app().test_client()
+    return client
+
+
+def test_gets_hello_world(client):
+    # arrange
+    expected = {"message": "Hello World!"}
+
+    # act
+    response = client.get('/')
+
+    # assert
+    assert_that(response.get_json()).is_equal_to(expected)
+
